d1abc29bc4cd4e0cfa23fd73f97ce38a13cf9627
DoubleNNs <nnamdi.anyanwu@gmail.com>
2015-11-07 18:05:19 +0000

    Fixed typos. Added whitespace to output for readibility.

--- books/automateTheBoringStuff/picnicTable.py
+++ books/automateTheBoringStuff/picnicTable.py
@@ -5,7 +5,7 @@ def printPicnic(itemsDict, leftWidth, rightWidth):
     for i, j in itemsDict.items():
         print(i.ljust(leftWidth, '.') + str(j).rjust(rightWidth))
 
-picniceItems = {'sandwiches': 4, 'apples': 12, 'cups': 4 'cookies': 8000}
+picnicItems = {'sandwiches': 4, 'apples': 12, 'cups': 4, 'cookies': 8000}
 
-printPicnic(picnicItems, 12, 5)
+printPicnic(picnicItems, 12, 5); print()
 printPicnic(picnicItems, 20, 6)
