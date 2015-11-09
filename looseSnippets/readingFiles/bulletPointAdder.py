e63c3b8dc072a086dcaf4ca7252699e1e268796d
DoubleNNs <nnamdi.anyanwu@gmail.com>
2015-11-09 13:45:11 +0000

    Test bed for reading from files

---
+++ looseSnippets/readingFiles/bulletPointAdder.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+
+# Adds asterisks as bullet points to the beginning of each line
+# of text from the file specified.
+
+
+'''
+Todo:
+    * Read text from file into variable -- sys.argv to specify file
+    * Print the 1 line string that was read in
+    * Split the lines by \n but store in single var
+    * Print the new var -- should be a list
+    * Iterate through the list and print the lines 'normally'
+'''
