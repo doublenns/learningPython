b0057b51d335f2af1dbebe70f7b43fb5c2856c26
DoubleNNs <nnamdi.anyanwu@gmail.com>
2015-11-09 00:04:06 +0000

    Added 1 comment

--- projectEuler/problem_001.py
+++ projectEuler/problem_001.py
@@ -14,6 +14,7 @@ multiples of 3 or 5 below 1000.
 valueSum = 0
 
 for i in range(1000):
+    # Determine if iteration is multiple of 3 or 5.
     if not (i % 3) or not (i % 5):
         valueSum += i
 
