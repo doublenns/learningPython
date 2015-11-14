0b7555afbea9e92425bc61d28a55986603ef3b54
DoubleNNs <nnamdi.anyanwu@gmail.com>
2015-11-14 20:18:27 +0000

    FizzBuzz!

---
+++ pythonLab/fizzBuzz.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+
+# FizzBuzz!
+# Prints the numbers from 1 to 10 with "fizz" for
+# multiples of 3, and "buzz" for multiples of 5
+
+
+for i in range(1, 101):
+    if not (i % 3) and not (i % 5):
+        print("FizzBuzz")
+    elif (i % 3) == 0:
+        print("Fizz")
+    elif not (i % 5):
+        print("Buzz")
+    else:
+        print(i)
+
+
+#values = set(range(3,101,3)) | set(range(5,101,5))
+#print(values)
