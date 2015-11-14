409c1542bdd03b5b4453c425468449093f9f1aed
DoubleNNs <nnamdi.anyanwu@gmail.com>
2015-11-14 20:56:54 +0000

    Version of FizzBuzz that uses a generator

---
+++ pythonLab/fizzBuzz_generator.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+
+# FizzBuzz!
+# Prints the numbers from 1 to 10 with "fizz" for
+# multiples of 3, and "buzz" for multiples of 5
+
+
+
+def generate_fizz_buzz(limit):
+    i = 0
+    while i < limit:
+        if not (i % 3) and not (i % 5):
+            yield "FizzBuzz"
+        elif (i % 3) == 0:
+            yield "Fizz"
+        elif not (i % 5):
+            yield  "Buzz"
+        else:
+            yield i
+
+        i += 1
+
+fizzbuzzer = generate_fizz_buzz(100)
+for output in fizzbuzzer:
+    print(output)
+
+# Generators help avoid stack overflow by minimizing memory usage
