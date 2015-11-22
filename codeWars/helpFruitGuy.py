6640d2226f1299965619c6a5027edee30d762924
DoubleNNs <nnamdi.anyanwu@gmail.com>
2015-11-22 02:05:09 +0000

    Method to remove "rotten" from fruit in list

---
+++ codeWars/helpFruitGuy.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+
+'''
+Fruit guy has a bag_of_fruits of fruits (array of strings) where some fruits are rotten.
+He wants to replace all the rotten fruits with good ones.
+For example, given the array ["apple","rottenBanana","apple"] the replaced
+array should be ["apple","banana","apple"].
+Implement method that will take as an argument an array of strings containing
+fruits & return array of strings with only good fruit.
+'''
+
+'''
+Note: If the array is null or empty you should return empty array ([]).
+The returned array should be in LOWER case.
+'''
+
+
+bag = ["rottenApple", "rottenBanana", "Catelope", "rottenPineapple", "Kiwi"]
+
+
+def remove_rotten(bag_of_fruits):
+    for i in range(len(bag_of_fruits)):
+        if "rotten" in bag_of_fruits[i]:
+            bag_of_fruits[i] = bag_of_fruits[i][6:]
+        bag_of_fruits[i] = bag_of_fruits[i].lower()
+
+    return(bag_of_fruits)
+
+
+print(remove_rotten(bag))
