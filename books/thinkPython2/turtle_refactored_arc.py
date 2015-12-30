aecd0e72d01c73ed9003d5d15c7714224df7fa88
DoubleNNs <nnamdi.anyanwu@gmail.com>
2015-12-30 16:49:41 +0100

    Refactored turtle_arc.py to use altered interface and polyline() function

---
+++ books/thinkPython2/turtle_refactored_arc.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+
+import turtle
+import sys
+import math
+
+
+def polyline(t, n, length, angle):
+    for i in range(n):
+        t.fd(length)
+        t.lt(angle)
+
+
+def arc(t, r, angle):
+    arc_length = 2 * math.pi * r * angle / 360
+    n = int(arc_length / 3) + 1
+    step_length = arc_length / n
+    step_angle = float(angle) / n
+    polyline(t, n, step_length, step_angle)
+
+
+bob = turtle.Turtle()
+try:
+    arc(bob, r=int(sys.argv[1]), angle=int(sys.argv[2]))
+except Exception:
+    print("You didn't enter all the required parameters to run the script")
+    sys.exit()
+
+turtle.mainloop()
