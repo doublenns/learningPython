abc7d2331f29737567d7899396cbe8aa133cfcd2
DoubleNNs <nnamdi.anyanwu@gmail.com>
2015-12-30 18:31:35 +0100

    Edited try/except to include only necessary code, making troubleshooting easier

--- books/thinkPython2/turtle_refactored_polygon.py
+++ books/thinkPython2/turtle_refactored_polygon.py
@@ -15,11 +15,14 @@ def polygon(t, n, length):
     polyline(t, n, length, angle)
 
 
-bob = turtle.Turtle()
 try:
-    polygon(bob, length=int(sys.argv[1]), n=int(sys.argv[2]))
+    length = int(sys.argv[1])
+    n = int(sys.argv[2])
 except Exception:
     print("You didn't enter all the required parameters to run the script")
     sys.exit()
 
+bob = turtle.Turtle()
+# Use of keyword arguments allow the input vars to be in an arbitrary order
+polygon(bob, length=length, n=n)
 turtle.mainloop()
