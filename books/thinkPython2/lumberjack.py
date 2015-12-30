7f0780b5dc8c4be618af11af479c25748f02dc93
Nnamdi Anyanwu <doublenns@Tiffanys-MacBook-Air.local>
2015-12-30 14:10:15 +0100

    lumberjack.py - demo of using nested functions

---
+++ books/thinkPython2/lumberjack.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+
+
+def print_lyrics():
+    print("I'm a lumberjack, and I'm okay.")
+    print('I sleep all night and I work all day.')
+
+
+def repeat_lyrics():
+    print_lyrics()
+    print_lyrics()
+
+
+repeat_lyrics()
