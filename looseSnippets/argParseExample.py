cc8c858fa491a1ba1becaf24661fc8995f8fdb04
doublenns <nnamdi.anyanwu@gmail.com>
2016-05-31 22:20:17 +0000

    Added "action='store true'" for verbosity arg

--- looseSnippets/argParseExample.py
+++ looseSnippets/argParseExample.py
@@ -8,16 +8,23 @@ import argparse
 
 
 parser = argparse.ArgumentParser()
-parser.add_argument("-n", "--name",
-    help = "Input a string for the script to use as name.")
+
 oses = ["Linux", "L",
     "Windows", "Win", "W",
     "Solaris", "SunOS", "S"]
+
+parser.add_argument("-n", "--name",
+    help = "Input a string for the script to use as name.")
 parser.add_argument("-o", "--os", type = str.capitalize, choices=oses,
     help = "Give the script an OS.")
-parser.add_argument("-p", "--port", type=int, 
+parser.add_argument("-p", "--port", type=int,
     help = "Input a number for the script to use as the port.")
-parser.add_argument("-v", "--verbosity",)
+parser.add_argument("-v", "--verbosity", action="store_true",
+    help = "Turn script verbosity on")
 
 args = parser.parse_args()
+
+if args.verbosity:
+    print("Verbosity has been turned ON")
+    print("The arguments passed to this script are: ", end="")
 print(args)
