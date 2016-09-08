1601c1a8067bd5c0ad8865de501227a442115552
DoubleNNs <nnamdianyanwu@gmail.com>
2016-09-08 13:07:03 -0400

    Test connection to PostgreSQL DB living in Vagrant box

--- sqlalchemy/sqlalchemy_quick_start.py
+++ sqlalchemy/sqlalchemy_quick_start.py
@@ -6,3 +6,26 @@ import sqlalchemy
 # Postgres installed on Vagrant box, w/ PSQL port 5432 forwarded -> local 5432
 # Script will probably need to be written as if it's connecting to a local
 #   PSQL instance, but the DB configured for remote connections
+
+
+def connect(user, password, db, host="localhost", port=5432):
+    '''
+    Returns a connection and metadata object
+    '''
+    # Connect w/ the help of the PostgreSQL URL
+    # postgresql://federer:grandestslam@localhost:5432/tennis
+    url = "postgresql://{}:{}@{}:{}/{}"
+    url = url.format(user, password, host, port, db)
+
+    # The return value of the create_enginer() is our connection object
+    con = sqlalchemy.create_engine(url, client_encoding="utf8")
+
+    # We then bind the connection to MetaData()
+    meta = sqlalchemy.MetaData(bind=con, reflect=True)
+
+    return con, meta
+
+con, meta = connect("federer", "grandestslam", "tennis")
+
+print con
+print meta
