21395a61504956e8c0cca23e98b2e72ca925d63f
DoubleNNs <nnamdianyanwu@gmail.com>
2020-07-17 09:13:53 -0500

    Add Automate the Boring Stuff with Python dir

---
+++ books/automateTheBoringStuff/hello_world.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+
+print("Hello World!")
+print("What is your name?")
+userName = input()
+print("It is good to meet you, " + userName)
+print("The length of your name is: " + str((len(userName))))
+print("What is your age?")
+userAge = input()
+print("You will be " + str(int(userAge) + 1) + " in one year.")
