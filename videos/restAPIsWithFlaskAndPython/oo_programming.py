1ccd4e1b5f8d6fee4382879b0ef184a441a54d10
DoubleNNs <nnamdianyanwu@gmail.com>
2020-07-21 11:00:25 -0500

    Add example of class composition

--- videos/restAPIsWithFlaskAndPython/oo_programming.py
+++ videos/restAPIsWithFlaskAndPython/oo_programming.py
@@ -39,3 +39,25 @@ printer.print(20)
 print(printer)
 printer.disconnect()
 printer.print(30)
+
+
+# Example of class composition
+class BookShelf:
+    def __init__(self, *books):
+        self.books = books
+
+    def __str__(self):
+        return f"BookShelf with {len(self.books)} books"
+
+
+class Book:
+    def __init__(self, name):
+        self.name = name
+
+    def __str__(self):
+        return f"Book {self.name}"
+
+book1 = Book("Animorphs")
+book2 = Book("Python 101")
+shelf = BookShelf(book1, book2)
+print(shelf)
